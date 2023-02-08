6b5e3c8b9eaf8745627b8a007fd13441b28dec6b
karankharole <karankharole2000@gmail.com>
2023-02-08 22:14:56 +0530

    read wrute csv file problem

---
+++ CSVHandler.cs
@@ -0,0 +1,51 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jSon_And_CSV_Problem
+{
+    internal class CSVOperations
+    {
+        public static bool IsFileExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+                return false;
+            }
+        }
+        public static void WriteRecordsInCSVFile(string path, PersonInput input)
+        {
+            if (IsFileExists(path))
+            {
+                List<PersonInput> list = new List<PersonInput>();
+                list.Add(input);
+                StreamWriter stream = new StreamWriter(path);
+                CsvWriter csv = new CsvWriter(stream, CultureInfo.InvariantCulture);
+                csv.WriteRecords<PersonInput>(list);
+                stream.Flush();
+            }
+        }
+        public static void ReadRecordsInCSVFile(string path)
+        {
+            if (IsFileExists(path))
+            {
+                StreamReader stream = new StreamReader(path);
+                CsvReader csv = new CsvReader(stream, CultureInfo.InvariantCulture);
+                List<PersonInput> list = csv.GetRecords<PersonInput>().ToList();
+                foreach (PersonInput record in list)
+                {
+                    Console.WriteLine(record);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace jSon_And_CSV_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cSVPath = @"D:\jSon_And_CSV_Problem\ContactjSon.json";
+
+            PersonInput input = new PersonInput();
+            Console.WriteLine("\nEnter your First Name : ");
+            input.fName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name : ");
+            input.lName = Console.ReadLine();
+            Console.WriteLine("Enter your Address : ");
+            input.address = Console.ReadLine();
+            Console.WriteLine("Enter your City Name : ");
+            input.city = Console.ReadLine();
+            Console.WriteLine("Enter your State Name : ");
+            input.state = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code : ");
+            input.zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number : ");
+            input.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Address: ");
+            input.email = Console.ReadLine();
+
+            
+
+            CSVOperations.WriteRecordsInCSVFile(cSVPath, input);
+            Console.WriteLine("\n\nRecords present in CSV file are : \n");
+            CSVOperations.ReadRecordsInCSVFile(cSVPath);
+            Console.ReadLine();
+        }
+    }
+}
